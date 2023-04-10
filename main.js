93f64dc55f6a700ea7a190d44bbdffabbbdd1c49
ramiromartinez <ramartinez1990@gmail.com>
2023-04-10 10:33:32 -0300

    Primer commit JS1

---
+++ main.js
@@ -0,0 +1,27 @@
+/*1*/
+/*const numero = prompt("Ingrese un número");
+if (numero % 2 === 0){
+    console.log ("El número " + numero + " es par.");
+} else {
+    console.log ("El número " + numero + " es impar.");
+}*/
+
+/*2*/
+/*const numero1 = prompt ("Ingrese un número");
+const numero2 = prompt ("Ingrese otro número");
+if (numero1 > numero2){
+    console.log (numero1 + " es mayor que " + numero2);
+} else {
+    console.log (numero1 + " es menor que " + numero2);
+}  FALTA QUE TIRE "SON IGUALES"*/
+
+/*3*/
+/*const numero = prompt("Ingrese un número y le diré si es multiplo de 5.");
+if (numero % 5 === 0){
+    console.log ("El número " + numero + " es multiplo de 5.");
+} else {
+    console.log ("El número " + numero + " no es muliplo de 5.");
+}*/
+
+/*4*/
+const numero = prompt ("Ingrese un número");
