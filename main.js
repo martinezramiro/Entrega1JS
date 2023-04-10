af20e8e62687c0cc7a7de0062f4875b60959cf74
ramiromartinez <ramartinez1990@gmail.com>
2023-04-10 11:41:14 -0300

    pendiente ejercicio N°2

--- main.js
+++ main.js
@@ -9,11 +9,21 @@ if (numero % 2 === 0){
 /*2*/
 /*const numero1 = prompt ("Ingrese un número");
 const numero2 = prompt ("Ingrese otro número");
-if (numero1 > numero2){
-    console.log (numero1 + " es mayor que " + numero2);
-} else {
-    console.log (numero1 + " es menor que " + numero2);
-}  FALTA QUE TIRE "SON IGUALES"*/
+
+switch (true) {
+    case numero1 > numero2:
+        console.log (numero1 + " es mayor que " + numero2);
+        break;
+    case numero1 < numero2:
+        console.log (numero1 + " es menor que " + numero2);
+        break;
+    case numero1 === numero2:
+        console.log (numero1 + " es igual que " + numero2);
+        break;
+    default:
+        console.log ("Ingresa sólo numeros.");
+        break;
+} TERMINAR DE DESARROLLAR*/
 
 /*3*/
 /*const numero = prompt("Ingrese un número y le diré si es multiplo de 5.");
@@ -24,4 +34,3 @@ if (numero % 5 === 0){
 }*/
 
 /*4*/
-const numero = prompt ("Ingrese un número");
