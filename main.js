a50e63f1ee2b9cc6f115456621566c822e76640e
ramiromartinez <ramartinez1990@gmail.com>
2023-04-11 11:48:23 -0300

    Faltan ejercicios 5,6 y 8

--- main.js
+++ main.js
@@ -11,19 +11,19 @@ if (numero % 2 === 0){
 const numero2 = prompt ("Ingrese otro número");
 
 switch (true) {
-    case numero1 > numero2:
+    case +numero1 > +numero2:
         console.log (numero1 + " es mayor que " + numero2);
         break;
-    case numero1 < numero2:
+    case +numero1 < +numero2:
         console.log (numero1 + " es menor que " + numero2);
         break;
-    case numero1 === numero2:
+    case +numero1 === +numero2:
         console.log (numero1 + " es igual que " + numero2);
         break;
     default:
         console.log ("Ingresa sólo numeros.");
         break;
-} TERMINAR DE DESARROLLAR*/
+}*/
 
 /*3*/
 /*const numero = prompt("Ingrese un número y le diré si es multiplo de 5.");
@@ -34,3 +34,34 @@ if (numero % 5 === 0){
 }*/
 
 /*4*/
+/*let arrayNumero = prompt ("Ingrese un número");
+for (let i = -1 ; i < arrayNumero ; i++) {
+    
+    console.log (i+1);
+}*/
+
+/*5*/
+let palabra = prompt ("Escriba una palabra");
+let numero = prompt ("Escriba un número");
+
+
+/*6*/
+/*let arrayParaRecorrer = ["Hola",1,2,3,4,5,"Chau"];
+for (let i = 0 ; i < arrayParaRecorrer.length ; i++) {
+    const element = arrayParaRecorrer [i];
+    console.log (element);
+}*/
+
+/*7*/
+/*let miArray = [1,2,3,4,5,6,7,8,9,10];
+console.log (miArray[0]);
+console.log (miArray[1]);
+console.log (miArray[2]);
+console.log (miArray[3]);
+console.log (miArray[5]);
+console.log (miArray[6]);
+console.log (miArray[7]);
+console.log (miArray[8]);
+console.log (miArray[9]);*/
+
+/*8*/
